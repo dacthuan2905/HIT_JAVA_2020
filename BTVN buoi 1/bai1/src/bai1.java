901b615b656083d8128c95dded013c4dc71112fb
dacthuan2905 <caodacthuan2001@gmail.com>
2020-09-25 11:16:47 +0700

    BTVN buoi 1

---
+++ BTVN buoi 1/bai1/src/bai1.java
@@ -0,0 +1,13 @@
+public class bai1 {
+    public static void main(String[] args) {
+        int m = 12;
+        int n = 3;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.printf("*");
+            }
+            System.out.println("");
+        }
+
+    }
+}
